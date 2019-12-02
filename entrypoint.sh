f544dc64ffcaf80849d72c1c20eef16b74057a4f
Matt Shin <matthew.shin@metoffice.gov.uk>
2019-12-02 23:17:59 +0000

    Try $HOME/myenv instead of GITHUB_WORKSPACE

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 set -eu
 set -o pipefail
 
-if ! conda env list 1>'/dev/null' | grep -F "${GITHUB_WORKSPACE}"; then
-    conda create -y -p "${GITHUB_WORKSPACE}"
+if ! conda env list 1>'/dev/null' | grep -F "${HOME}/myenv"; then
+    conda create -y -p "${HOME}/myenv"
     echo '. /opt/conda/etc/profile.d/conda.sh' >> ~/.bashrc
     . '/opt/conda/etc/profile.d/conda.sh'
 fi
 set -x
 if (($# != 0)); then
-    conda activate "${GITHUB_WORKSPACE}"
+    conda activate "${HOME}/myenv"
     "$@"
 fi
 exit
