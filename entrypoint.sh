9f3f13d27281c90e290fd884fe3ecec9cc0fa9b5
Matt Shin <matthew.shin@metoffice.gov.uk>
2019-12-03 10:25:11 +0000

    Don't use conda env list to detect environment
    Because it does not know about the prefix environment.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,15 @@
 set -eu
 set -o pipefail
 
-ENV_PREFIX="${GITHUB_WORKSPACE}/myenv"
-
-env_invoke() {
-    . '/opt/conda/etc/profile.d/conda.sh'
-    conda activate "${ENV_PREFIX}"
-    "$@"
-    find "${ENV_PREFIX}" -type f -ls
-}
-
 main() {
-    if ! conda env list 1>'/dev/null' | grep -F "${ENV_PREFIX}"; then
-        (conda create -y -p "${ENV_PREFIX}")
+    local ENV_PREFIX="${GITHUB_WORKSPACE}/myenv"
+    . '/opt/conda/etc/profile.d/conda.sh'
+    if ! conda activate "${ENV_PREFIX}" 2>'/dev/null'; then
+        conda create -y -p "${ENV_PREFIX}"
     fi
     if (($# != 0)); then
-        (env_invoke "$@")
+        "$@"
+        find "${GITHUB_WORKSPACE}" -type d
     fi
 }
 
