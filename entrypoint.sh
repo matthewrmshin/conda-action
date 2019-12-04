fe5448cbf60efb4bbf6e71f2e3a0e9f2708af9e7
Matt Shin <matthew.shin@metoffice.gov.uk>
2019-12-04 10:40:59 +0000

    Turn off `set -u` when running Conda ...

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,12 @@ main() {
     set -x
     local ENV_PREFIX="${GITHUB_WORKSPACE}/myenv"
     . '/opt/conda/etc/profile.d/conda.sh'
+    set +u
     if ! conda activate "${ENV_PREFIX}"; then
         conda create -y -p "${ENV_PREFIX}"
         conda activate "${ENV_PREFIX}"
     fi
+    set -u
     if (($# != 0)); then
         "$@"
     fi
