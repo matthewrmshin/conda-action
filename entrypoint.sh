32b667b0c5234b47eab0f0de22ba227042034d1c
Matt Shin <matthew.shin@metoffice.gov.uk>
2019-12-04 10:59:39 +0000

    Modify location of environment
    Use `.myenv` so it is ignored by normal globbing.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@ set -eu
 set -o pipefail
 
 main() {
-    set -x
-    local ENV_PREFIX="${GITHUB_WORKSPACE}/myenv"
+    local ENV_PREFIX="${GITHUB_WORKSPACE}/.myenv"
     . '/opt/conda/etc/profile.d/conda.sh'
     set +u
     if ! conda activate "${ENV_PREFIX}"; then
